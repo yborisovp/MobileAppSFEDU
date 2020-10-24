d8c1357b0a10f9d26ee41b7c52dd20704a4de529
izmailov-vlad <izmailov.vlad2001@gmail.com>
2020-10-24 21:04:37 +0300

    failed v2.6 not working change of tables

--- app/src/main/java/com/example/timetable/Parse.java
+++ app/src/main/java/com/example/timetable/Parse.java
@@ -13,24 +13,29 @@ import java.net.URL;
 
 import java.util.ArrayList;
 
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 public class Parse implements Callable<ArrayList<TimeList>> {
     private ArrayList<TimeList> lists;
-    private int dayOfWeek;
-    private String url = "http://165.22.28.187/schedule-api/?group="+"48"+".htm&week="+"7";
-    private JSONArray arraylesson;
+    private int day;
+    private String week;
+    private String url;
+    private Map<String, Integer> daysOfWeek;
 
 
-    Parse(ArrayList<TimeList> lists, int dayOfWeek) {
+    Parse(ArrayList<TimeList> lists, int day, String week) {
         this.lists = lists;
-        this.dayOfWeek = dayOfWeek;
+        this.day = day;
+        this.week = week;
+
     }
 
 
     @Override
     public ArrayList<TimeList> call() throws Exception {
         try {
+            url = "http://165.22.28.187/schedule-api/?group="+"48"+".htm&week=8";
             URL obj = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
 
@@ -50,8 +55,7 @@ public class Parse implements Callable<ArrayList<TimeList>> {
             org.json.JSONObject parsedObject = new org.json.JSONObject(jsonText); //Здесь парсим
             JSONArray top = parsedObject.getJSONObject("table").getJSONArray("table");
 
-            if (dayOfWeek != 1) {
-                arraylesson = (JSONArray) top.get(dayOfWeek);
+            JSONArray arraylesson = (JSONArray) top.get(this.day);
 
                 lists.add(new TimeList("08:00 - 09:35", arraylesson.get(1).toString()));
                 lists.add(new TimeList("09:50 - 11:25", arraylesson.get(2).toString()));
@@ -60,7 +64,7 @@ public class Parse implements Callable<ArrayList<TimeList>> {
                 lists.add(new TimeList("15:50 - 17:25", arraylesson.get(5).toString()));
                 lists.add(new TimeList("17:40 - 19:15", arraylesson.get(6).toString()));
                 lists.add(new TimeList("19:30 - 21:05", arraylesson.get(7).toString()));
-            }
+
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
